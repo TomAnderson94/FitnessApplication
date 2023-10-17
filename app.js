77bd69eab6ecf39db21e5ec9595516db5de96df7
TomAnderson94 <137895998+TomAnderson94@users.noreply.github.com>
2023-10-17 12:34:43 +0100

    Adding Exercises and ExerciseTypes Controllers

--- app.js
+++ app.js
@@ -10,12 +10,15 @@ const app = new express();
 
 // Controllers ----------------------------------------------
 const exercisesController = async (req,res) => {
+    const id = req.params.ExerciseID; // Undefined in the case of /api/exercises endpoint
     // Build SQL 
     const table = 'Exercises';
+    const whereField = 'ExerciseID';
     const fields = ['ExerciseID', 'ExerciseName', 'ExerciseExerciseTypeID', 'ExerciseURL'];
-    const extendedTable = `${table} LEFT JOIN ExerciseTypes ON Exercises.ExerciseExerciseTypeID=ExerciseTypeID`;
+    const extendedTable = `${table} LEFT JOIN ExerciseTypes ON Exercises.ExerciseExerciseTypeID=ExerciseTypes.ExerciseTypeID`;
 //  const extendedFields;
-    const sql = `SELECT ${fields} FROM ${extendedTable}`;
+    let sql = `SELECT ${fields} FROM ${extendedTable}`;
+    if (id) sql += ` WHERE ${whereField}=${id}`;
     // Execute query
     let isSuccess = false;
     let message = "";
@@ -35,9 +38,72 @@ const exercisesController = async (req,res) => {
     // Responses
     isSuccess
     ? res.status(200).json(result)
-    : res.status(400).json({ message: message });
+    : res.status(400).json({ message });
  };
 
+
+ const exercisesOfTypeController = async (req,res) => {
+    const id = req.params.ExerciseExerciseTypeID; 
+    // Build SQL 
+    const table = 'Exercises';
+    const whereField = 'ExerciseExerciseTypeID';
+    const fields = ['ExerciseID', 'ExerciseName', 'ExerciseExerciseTypeID', 'ExerciseURL'];
+    const extendedTable = `${table} LEFT JOIN ExerciseTypes ON Exercises.ExerciseExerciseTypeID=ExerciseTypes.ExerciseTypeID`;
+//  const extendedFields;
+    const sql = `SELECT ${fields} FROM ${extendedTable} WHERE ${whereField}=${id}`;
+    // Execute query
+    let isSuccess = false;
+    let message = "";
+    let result = null;
+    try {
+        [result] = await database.query(sql);
+        if(result.length === 0) message = 'No record(s) found';
+        else {
+            isSuccess = true;
+            message = 'Record(s) successfully found';
+        }
+    }
+    catch (error) {
+        message = `Failed to execute query: ${error.message}`;
+    }
+    
+    // Responses
+    isSuccess
+    ? res.status(200).json(result)
+    : res.status(400).json({ message });
+ };
+
+ const exerciseTypesController = async (req,res) => {
+    const id = req.params.ExerciseTypeID;
+    // Build SQL 
+    const table = 'ExerciseTypes';
+    const whereField = 'ExerciseTypeID';
+    const fields = ['ExerciseTypeID', 'ExerciseTypeName'];
+//  const extendedTable = `${table} LEFT JOIN ExerciseTypes ON Exercises.ExerciseExerciseTypeID=ExerciseTypes.ExerciseTypeID`;
+//  const extendedFields;
+    let sql = `SELECT ${fields} FROM ${table}`;
+    if (id) sql += ` WHERE ${whereField}=${id}`;
+    // Execute query
+    let isSuccess = false;
+    let message = "";
+    let result = null;
+    try {
+        [result] = await database.query(sql);
+        if(result.length === 0) message = 'No record(s) found';
+        else {
+            isSuccess = true;
+            message = 'Record(s) successfully found';
+        }
+    }
+    catch (error) {
+        message = `Failed to execute query: ${error.message}`;
+    }
+    
+    // Responses
+    isSuccess
+    ? res.status(200).json(result)
+    : res.status(400).json({ message });
+ };
 
 /*const helloController = (req,res) => res.send("Hi, my name is Server");
 const addController = (req,res) => {
@@ -55,6 +121,13 @@ const addController = (req,res) => {
 
 // Endpoints ------------------------------------------------
 app.get('/api/exercises', exercisesController);
+app.get('/api/exercises/:ExerciseID', exercisesController);
+app.get('/api/exercises/exercise-types/:ExerciseExerciseTypeID', exercisesOfTypeController);
+
+app.get('/api/exerciseTypes', exerciseTypesController);
+app.get('/api/exerciseTypes/:ExerciseTypeID', exerciseTypesController);
+
+
 
 /*app.get('/hello', helloController);
 app.get('/add/:var1,:var2', addController);*/
