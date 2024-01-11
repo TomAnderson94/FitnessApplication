2c55ed90327d7dee6d72868435d3c6b378fe9a94
TomAnderson94 <137895998+TomAnderson94@users.noreply.github.com>
2024-01-11 13:02:47 +0000

    Login Controller

--- app.js
+++ app.js
@@ -252,6 +252,53 @@ const deleteExerciseRecordController = async (req, res) => {
   }
 };
 
+/*
+const getUserByUsername = async (username) => {
+  const sql = 'SELECT UserID, FirstName, LastName, UserRole FROM Users WHERE username = ?';
+  const [users] = await database.query(sql, [username]);
+  return users[0];
+};
+
+const registerUserController = async (req, res) => {
+  try {
+    const { username, password, role } = req.body;
+    const sql = 'INSERT INTO Users (Username, Password, UserRole) VALUES (?, ?, ?)';
+    await database.query(sql, [username, password, role]);
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error('Registration error:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+*/
+
+
+const loginController = async (req, res) => {
+  try {
+    const { username } = req.body;
+
+    // Check to assign user type
+    let userType;
+    if (username.includes('.exerciser')) {
+      userType = 'exerciser';
+    } else if (username.includes('.trainer')) {
+      userType = 'trainer';
+    } else if (username.includes('.admin')) {
+      userType = 'admin';
+    } else {
+      return res.status(401).json({ message: 'Login failed' });
+    }
+
+    // Respond with the user type
+    res.json({ userType });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+
+
 
   
 
@@ -271,6 +318,9 @@ app.post('/api/userExercises', recordExerciseController);
 
 app.put('/api/userExercises/:UserExerciseID/:UserUserID', updateExerciseRecordController);
 app.delete('/api/userExercises/:UserExerciseID/:UserUserID', deleteExerciseRecordController);
+
+// Login
+app.post('/api/login', loginController);
 
 
 
